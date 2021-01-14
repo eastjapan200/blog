6df7b053784d19d79e3e248b6b3b919833033aa7
eastjapan200 <66190096+eastjapan200@users.noreply.github.com>
2021-01-14 18:28:48 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+
+<template>
+ <ul>
+  <li v-for="content in contents" :key="content.id">
+   <nuxt-link :to="`/${content.id}`">
+    {{ content.title }}
+   </nuxt-link>
+  </li>
+ </ul>
+</template>
+
+
+<script>
+import axios from 'axios'
+export default {
+ async asyncData(){
+  const{ data } = await axios.get(
+    'https://t1964101.microcms.io/api/v1/blog',
+    {
+    headers:{'X-API-KEY':'36847a75-600f-438f-b270-1a08a33fc637'}
+    }
+  )
+  return data
+ }
+}
+</script>
+
+
